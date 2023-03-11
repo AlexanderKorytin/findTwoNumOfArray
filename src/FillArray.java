ec64d15705ee6c325e9de025586dbb08535d89d8
aleksandrkorytin <al.corytin@yandex.ru>
2023-03-11 12:44:56 +0300

    done. Looks for two numbers in a user-supplied array that add up to the user-supplied number.

---
+++ src/FillArray.java
@@ -0,0 +1,38 @@
+package src;
+import java.util.Scanner;
+
+public class FillArray implements FillArrayFromStr{
+    String[] strMass;
+    String str;
+    int request;
+    Scanner scan = new Scanner(System.in);
+    public int request () {
+        while (true) {
+            System.out.println("Enter an integer, the terms of which will be searched in the array?");
+            if (scan.hasNextInt()) {
+                request = scan.nextInt();
+                break;
+            } else {
+                System.out.println("Error!!! Enter an integer, the terms of which will be searched in the array!");
+            }
+        }
+        scan.close();
+        return request;
+    }
+
+    @Override
+    public Integer[] ArrayFromString(String str) {
+        strMass = str.split(" ");
+        Integer[] mass = new Integer[strMass.length];
+        for (int i = 0; i < strMass.length; i++) {
+            try {
+                mass[i] = Integer.parseInt(strMass[i]);
+            } catch (NumberFormatException e) {
+                mass[i] = 0;
+            }
+        }
+        return mass;
+    }
+}
+
+
