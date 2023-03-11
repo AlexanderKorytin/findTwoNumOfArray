c9c72f691f6ce4c2c0b3b6131fddc5e6ec321deb
aleksandrkorytin <al.corytin@yandex.ru>
2023-03-11 12:53:44 +0300

    done. Looks for two numbers in a user-supplied array that add up to the user-supplied number.

---
+++ src/FillArrayFromStr.java
@@ -0,0 +1,5 @@
+package src;
+
+public interface FillArrayFromStr {
+    Integer [] ArrayFromString (String str);
+}
---
+++ src/SortArrays.java
@@ -0,0 +1,7 @@
+package src;
+
+public interface SortArrays<T> {
+    T[] sort(T[] mass);
+
+    T[] searchResult(T[] mass, T t);
+}
