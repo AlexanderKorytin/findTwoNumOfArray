dd7c2c75ff37919a09efccda9e12086f25e91ce3
aleksandrkorytin <al.corytin@yandex.ru>
2023-03-11 13:13:57 +0300

    исправлен мелкий недочет по неиспользуемым переменным

--- src/Main.java
+++ src/Main.java
@@ -16,7 +16,7 @@ public class Main {
         int result = fill.request(); // получаем из консоли число два слагаемых которого буде искать в массиве
 
         Integer[] sortMass = sortArrays.sort(mass);// сортируем массив по возрастанию, выводим его на экран
-        Integer[] resultMass = sortArrays.searchResult(sortMass, result);// ищем слагаемые, введенного ранее сисла
+        sortArrays.searchResult(sortMass, result);// ищем слагаемые, введенного ранее сисла
         // в мвссиве и выводи результирующий массив из двух чисел на экран
     }
 }
